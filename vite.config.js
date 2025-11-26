cbddf60647eef6665d54b9fc3e9d1733afe2dd18
neoaway2909 <neoaway42@gmail.com>
2025-11-26 23:16:41 +0700

    nn

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
+      jsxRuntime: 'automatic',   // ← บังคับให้ใช้ JSX runtime ใหม่
       babel: {
         plugins: ['styled-jsx/babel'],
       },
     }),
   ],
 })
+
